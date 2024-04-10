e04ce0a0d1a33540550726de2fe00428db64cdfc
aayushj567 <aayushjaiswal765@gmail.com>
2024-04-10 13:24:25 -0400

    add login logout firebase

--- WanderlustAppiOS/ViewController.swift
+++ WanderlustAppiOS/ViewController.swift
@@ -21,20 +21,9 @@ class ViewController: UIViewController {
     }
     
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        //MARK: handling if the Authentication state is changed (sign in, sign out, register)...
-        handleAuth = Auth.auth().addStateDidChangeListener{ auth, user in
-            if user == nil{
-                //self.showAlert(message: "Please sign in to continue")
-                
-            }else{
-                //MARK: the user is signed in...
-                self.onLoginTapped()
-            }
-        }
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,46 +32,62 @@ class ViewController: UIViewController {
         loginScreen.buttonLogin.addTarget(self, action: #selector(onLoginTapped), for: .touchUpInside)
         loginScreen.buttonRegister.addTarget(self, action: #selector(onRegistrationTapped), for: .touchUpInside)
         
+        handleAuth = Auth.auth().addStateDidChangeListener{ auth, user in
+            if user == nil{
+                //MARK: not signed in...
+                
+            } else {
+                //MARK: the user is signed in...
+                let mainScreen = CalendarViewController()
+                self.navigationController?.pushViewController(mainScreen, animated: true)
+            }
+        }
+        
     }
     
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        Auth.auth().removeStateDidChangeListener(handleAuth!)
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        Auth.auth().removeStateDidChangeListener(handleAuth!)
+//    }
+    
+    //MARK: sign-in logic for Firebase...
+    func signIntoFirebase(email: String, password: String){
+        //authenticating the user...
+        Auth.auth().signIn(withEmail: email, password: password, completion: {(result, error) in
+            if error == nil{
+                //user authenticated...
+                let mainScreen = CalendarViewController()
+                self.navigationController?.pushViewController(mainScreen, animated: true)
+            }else{
+                //alert that no user found or password wrong...
+                self.showAlert(title: "Error", message: "No user found with the provided credentials.")
+            }
+        })
     }
     
+    
+    // MARK: action when the login button is tapped...
     @objc func onLoginTapped(){
        
-//        guard let emailText = loginScreen.textFieldEmail.text, !emailText.isEmpty,
-//              let passwordText = loginScreen.textFieldPassword.text, !passwordText.isEmpty
-//        else {
-//            showAlert(message: "Please fill out all required fields.")
-//            return
-//        }
-//        
-//        guard isValidEmail(emailText) else {
-//            showAlert(message: "Invalid email. Please enter a valid email address.")
-//            return
-//        }
+        guard let emailText = loginScreen.textFieldEmail.text, !emailText.isEmpty,
+              let passwordText = loginScreen.textFieldPassword.text, !passwordText.isEmpty
+        else {
+            showAlert(title: "Error", message: "Please fill out all required fields.")
+            return
+        }
         
-        //let user = User(id:"", name: "", email: emailText, password: passwordText)
-        
-        //loginExistingUser(user:user)
-        //let mainScreen = NoteViewController()
-        //navigationController?.pushViewController(mainScreen, animated: true)
-        let mainScreen = CalendarViewController()
-//        // hide the navigation bar
-//        navigationController?.setNavigationBarHidden(true, animated: true)
-        navigationController?.pushViewController(mainScreen, animated: true)
-        
+        guard isValidEmail(emailText) else {
+            showAlert(title: "Error", message: "Invalid email. Please enter a valid email address.")
+            return
+        }
+        signIntoFirebase(email: emailText, password: passwordText)
     }
+    
     
     @objc func onRegistrationTapped(){
        
         let registrationScreen = RegistrationViewController()
-        //Setting up the contact object of display screen.
-        //Instead of sending the variables used a struct named Contact to send the details.
-        
         navigationController?.pushViewController(registrationScreen, animated: true)
     }
 
@@ -94,8 +99,8 @@ class ViewController: UIViewController {
     }
     
     
-    func showAlert(message: String) {
-        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+    func showAlert(title:String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
